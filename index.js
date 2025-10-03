bd6bce8cd1fccb476d6aaae06b76ea8a46bb1101
Normurodov Firdavs <108564450+Firdavs-Normurodov@users.noreply.github.com>
2025-10-03 16:07:37 +0500

    Add dotenv and cors dependencies; configure server to use dotenv and enable CORS

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const server = express();
 server.use(express.json());
+server.use(cors((origin = "*")));
+
 server.get("/", (_, res) => {
   res.send({ ok: true });
 });
 server.use("/posts", postRoute);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.info("server is running 3000");
 });
