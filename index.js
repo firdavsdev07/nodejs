0d9d9e40b6e1ebc963edb47d8281465cdac1cd10
Normurodov Firdavs <108564450+Firdavs-Normurodov@users.noreply.github.com>
2025-10-03 16:12:24 +0500

    Remove default origin setting in CORS middleware for improved security

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = express();
 server.use(express.json());
-server.use(cors((origin = "*")));
+server.use(cors());
 
 server.get("/", (_, res) => {
   res.send({ ok: true });
